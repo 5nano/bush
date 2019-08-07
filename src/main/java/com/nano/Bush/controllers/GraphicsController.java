11d45b0d2fbb1978bf4d1604f25086defc15056b
ayalaignacio <ignacio.ayala@despegar.com>
2019-08-07 20:34:26 -0300

    pom modified

--- src/main/java/com/nano/Bush/controllers/GraphicsController.java
+++ src/main/java/com/nano/Bush/controllers/GraphicsController.java
@@ -1,22 +1,29 @@
 package com.nano.Bush.controllers;
 
+import com.nano.Bush.model.GraphicDto;
 import com.nano.Bush.services.GraphicsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
+
+@Controller
+@RequestMapping("/bush")
 public class GraphicsController {
 
     @Autowired
     private GraphicsService graphicsService;
 
-    @GetMapping(value = "/bush/comparativeGraphic")
-    @ResponseBody
-    public String getComparativeInfoGraphic() {
-        System.out.println("Me pegaron");
-        return "graphicsService.getComparativeGraphicInfo()";
+
+    @RequestMapping(value = "/comparativeGraphic", method = RequestMethod.GET, produces = "application/json")
+    public @ResponseBody
+    ResponseEntity<List<GraphicDto>> getComparativeInfoGraphic() {
+        return new ResponseEntity<>(graphicsService.getComparativeGraphicInfo(), HttpStatus.OK);
     }
 
 
