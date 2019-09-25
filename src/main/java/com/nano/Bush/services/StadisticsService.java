695db8e4a7a5b3bfdf345b67433f89b79f2e7fcc
Matias Zeitune <matias.zeitune@despegar.com>
2019-09-25 00:26:05 -0300

    frequencies controller

--- src/main/java/com/nano/Bush/services/StadisticsService.java
+++ src/main/java/com/nano/Bush/services/StadisticsService.java
@@ -2,7 +2,6 @@ package com.nano.Bush.services;
 
 import com.nano.Bush.datasources.measures.ExperimentsDao;
 import com.nano.Bush.datasources.measures.MeasuresDao;
-import com.nano.Bush.model.BoxDiagramValues;
 import com.nano.Bush.model.Experiment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,10 +25,10 @@ public class StadisticsService {
 
     private static final Logger logger = LoggerFactory.getLogger(StadisticsService.class);
 
-    public BoxDiagramValues getBoxDiagramValuesExperiment(String experimentId) throws SQLException {
+    public Set<Double>  getYellowFrequenciesValuesExperiment(String experimentId) throws SQLException {
         ExperimentsDao experimentsDao = new ExperimentsDao();
 
-        BoxDiagramValues boxDiagramValues;
+        Set<Double> yellowFrequencies;
 
         try {
             Experiment experiment;
@@ -39,15 +38,11 @@ public class StadisticsService {
                     .map(measure -> measure.getObservations().getYellowFrequencies().getValue())
                     .collect(Collectors.toList());
             List<Double> allFrequenciesExperiment = frequencies.stream().flatMap(List::stream).collect(Collectors.toList());
-            Set<Double> yellowFrequencies = new HashSet<>(allFrequenciesExperiment);
-
-
-            boxDiagramValues = new BoxDiagramValues(0.0, 0.0, 0.0, 0.0, 0.0);
-
+            yellowFrequencies = new HashSet<>(allFrequenciesExperiment);
         } catch (SQLException e) {
             logger.error("Error al obtener el nombre del experimento, exception: " + e);
             throw new RuntimeException("Error al obtener el nombre del experimento, exception: " + e);
         }
-        return boxDiagramValues;
+        return yellowFrequencies;
     }
 }
