d12e02c15a32c2e2d4b3482b029f67cc3363f67c
lucasAmoroso <lucas.amoroso@despegar.com>
2019-10-12 16:42:24 -0300

    manage sessions

--- src/main/java/com/nano/Bush/controllers/ExceptionHandlerController.java
+++ src/main/java/com/nano/Bush/controllers/ExceptionHandlerController.java
@@ -1,10 +1,10 @@
 package com.nano.Bush.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,11 +13,7 @@ public class ExceptionHandlerController {
 
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   @ExceptionHandler(Exception.class)
-  public ModelAndView handleError(HttpServletRequest req, Exception ex) {
-    ModelAndView mav = new ModelAndView();
-    mav.addObject("exception", ex);
-    mav.addObject("url", req.getRequestURL());
-    mav.setViewName("error");
-    return mav;
+  public ResponseEntity<String> handleError(HttpServletRequest req, Exception ex) {
+    return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
