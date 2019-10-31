f034a2ee8571cf2cd64fed66e26e35d73ea5153f
Matias Zeitune <matias.zeitune@despegar.com>
2019-10-31 18:35:20 -0300

    first version sending email

---
+++ src/main/java/com/nano/Bush/services/SendEmailHTML.java
@@ -0,0 +1,53 @@
+package com.nano.Bush.services;
+
+/**
+ * Created by Matias Zeitune oct. 2019
+ **/
+
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class SendEmailHTML {
+    public static void main(String [] args) throws AddressException {
+
+        final String username = "5nano.consultas@gmail.com";
+        final String password = "ebxnjrpslhjtiobb";
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.socketFactory.port", "465");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("5nano.consultas@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse("matizeitune@gmail.com, matias.zeitune@despegar.com")
+            );
+            message.setSubject("Testing Gmail SSL");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n Please do not spam my email!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
